f5b916a1b6dfed7d24b00ae1df59c013db7658f5
Ben Straub <ben@straub.cc>
2016-07-08 18:12:09 -0700

    Partial OAuth flow

--- plugins/slack_app/app/controllers/slack_app_controller.rb
+++ plugins/slack_app/app/controllers/slack_app_controller.rb
@@ -1,7 +1,31 @@
+require 'slack'
+
 class SlackAppController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  before_filter do
+    @app_token = SlackIdentifier.app_token
+    Slack.configure do |config|
+      config.token = @app_token
+    end
+  end
+
   def oauth
+    @need_to_connect_app = @app_token.nil?
+    @need_to_connect_user = SlackIdentifier.find_by_user_id(current_user).nil?
+
+    if params[:code]
+      # Got an OAuth code, let's fetch our tokens
+      resp = Slack.oauth_access client_id: ENV['SLACK_CLIENT_ID'],
+                                client_secret: ENV['SLACK_CLIENT_SECRET'],
+                                code: params[:code],
+                                redirect_uri: url_for(only_path: false)
+      puts '!!!', resp, resp['access_token'], resp['user_id']
+      if @need_to_connect_app
+        @app_token = SlackIdentifier.create!(identifier: resp['access_token']).identifier
+      end
+      SlackIdentifier.create!(user_id: current_user, identifier: resp['user_id'])
+    end
   end
 
   def command
