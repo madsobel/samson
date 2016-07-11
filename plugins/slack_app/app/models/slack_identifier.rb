643155e5cdff152dbae85fd9870db1d9b94b1fb3
Ben Straub <ben@straub.cc>
2016-07-11 06:54:17 -0700

    try(:identifier)

--- plugins/slack_app/app/models/slack_identifier.rb
+++ plugins/slack_app/app/models/slack_identifier.rb
@@ -2,7 +2,6 @@ class SlackIdentifier < ActiveRecord::Base
   belongs_to :user
 
   def self.app_token
-    record = SlackIdentifier.find_by_user_id nil
-    record.nil? ? nil : record.identifier
+    SlackIdentifier.find_by_user_id(nil).try(:identifier)
   end
 end
