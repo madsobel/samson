661f0222b9a5e2eea0894c0efc4c14cb18279692
Ben Straub <ben@straub.cc>
2016-07-11 15:17:44 -0700

    Embryonic test suite

---
+++ plugins/slack_app/test/controllers/slack_app_controller_test.rb
@@ -0,0 +1,32 @@
+require_relative '../test_helper'
+
+SingleCov.covered!
+
+describe SlackAppController do
+  as_a_viewer do
+    describe 'oauth' do
+    end
+  end
+
+  describe 'incoming commands' do
+    it "raises if secret token doesn't match" do
+      assert_raises RuntimeError do
+        post :command, token: 'thiswontmatch'
+      end
+    end
+
+    describe 'without Slack linkage' do
+      it "returns a private error if the user isn't matched up" do
+        post :command, user_id: 'notconnected'
+        puts @response.body
+        assert "don't recognize you".in? @response.body
+      end
+    end
+
+    describe 'with Slack linkage' do
+      beforeEach do
+        # TODO: mock up a SlackIdentifier for one or more users
+      end
+    end
+  end
+end
